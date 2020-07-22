84dc20aa416f9637b0608c22fdaf81be437ed0fe
kunci115 <rino.alpin@gmail.com>
2020-07-22 20:32:06 +0700

    test pipetest

--- agile_dev/core_engine/tests.py
+++ agile_dev/core_engine/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_get_values_all(client):
+   url = reverse('list-values')
+   response = client.get(url)
+   assert response.status_code == 200
